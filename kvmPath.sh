ff716122d9525c1dc20a8b3be14624e31cbff638
Stephen-T-Homick <shomick87@gmail.com>
2018-01-13 12:40:28 -0500

    Committed January 13, 2018 @ 12:40 PM

--- kvmPath.sh
+++ kvmPath.sh
@@ -8,9 +8,6 @@ RESETTXT="\e[0m"
 GREENTXT="\e[1m\e[32m"
 REDTXT="\e[1m\e[31m"
 BLUETXT="\e[1m\e[34m"
-(command -v apt || printf "[X] You are currently not utilizing a Debian Based Linux distro. \n \n " ; cat /etc/issue;  exit 1)
-#sys variables
-cpuname=$(cat /proc/cpuinfo | grep 'model name' | uniq)
 
 if [ $(id -u) != 0 ] ; then
   printf "$REDTEXT[X]$RESETTXT Script must be run as root/sudo \n"
@@ -18,8 +15,13 @@ if [ $(id -u) != 0 ] ; then
   exit 1
 fi
 
+(command -v dpkg || printf "[X] You are currently not utilizing a Debian Based Linux distro. \n \n " ; cat /etc/issue;  exit 1)
+
 echo -e " $GREENTXT[*]$RESETTXT Debian[*] check"
 echo -e " $GREENTXT[*]$RESETTXT root[*] check."
+
+#sys variables
+cpuname=$(cat /proc/cpuinfo | grep 'model name' | uniq)
 
 read -p "If you are ready to install libvirt/kvm please press Enter. Or press CTRL+C to cancel"
 
@@ -37,8 +39,9 @@ if [ $(egrep -c '(vmx|svm)' /proc/cpuinfo) -lt '1' ] ; then
 fi
 
 if [ $(egrep -c '(vmx|svm)' /proc/cpuinfo) -gt '1' ] ; then
+  printf "We're good to go. You have $(egrep -c '(vmx|svm)' /proc/cpuinfo) cores. "
   printf "KVM Ready to install. \n \n"
-  read -p "Press Enter to continue install, ctrl+c to exit."
+  read -p "Press Enter to continue install, or ctrl+c to cancel."
   printf "Installing \n \n "
   kvm_install
 fi
@@ -50,7 +53,7 @@ fi
 kvm_install(){
   (apt-get -y update && apt-get -y install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils && apt-get -y install virt-manager) && printf "\n \n Please reboot machine to establish libvirt / qemu connection \n \n"
   kvm-ok
-  read -p "Reboot now :> (CTRL+C to quit, Enter to reboot)"
+  read -p "Reboot now :> (CTRL+C to cancel, Enter to reboot)"
   sudo reboot
 
 }
