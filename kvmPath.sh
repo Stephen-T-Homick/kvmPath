1c3a6a4450d0b6cbddc966f2b9d17bd8da4dc7cf
Stephen-T-Homick <shomick87@gmail.com>
2018-02-16 08:28:31 -0500

    Removed faulty exit 1

--- kvmPath.sh
+++ kvmPath.sh
@@ -20,7 +20,7 @@ if [ $(id -u) != 0 ] ; then
   exit 1
 fi
 
-(command -v dpkg || printf "[X] You are currently not utilizing a Debian Based Linux distro. \n \n " ; cat /etc/issue;  exit 1)
+(command -v dpkg || printf "[X] You are currently not utilizing a Debian Based Linux distro. \n \n " ; cat /etc/issue)
 
 echo -e " $GREENTXT[*]$RESETTXT Debian / Ubuntu[*] check"
 echo -e " $GREENTXT[*]$RESETTXT root[*] check."
